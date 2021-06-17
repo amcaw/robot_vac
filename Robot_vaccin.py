05f4ef1457f1e0683cb0066f2f43021f8117bae9
amcaw <42608053+amcaw@users.noreply.github.com>
2021-06-17 15:52:59 +0200

    Update Robot_vaccin.py

--- Robot_vaccin.py
+++ Robot_vaccin.py
@@ -1,9 +1,9 @@
 import pandas as pd
 df = pd.read_csv('https://epistat.sciensano.be/Data/COVID19BE_VACC.csv')
 df = pd.pivot_table(df, index = 'DATE', columns = 'DOSE', values = 'COUNT', aggfunc = 'sum')
-df['CumuleA'] = df['A'].cumsum().map('{0:g}'.format)
-df['CumuleB'] = df['B'].cumsum().map('{0:g}'.format)
-df['CumuleC'] = df['C'].cumsum().map('{0:g}'.format)
-df['Dose1'] = df['CumuleA'] + df['CumuleC']
-df['Dose2'] = df['CumuleB'] + df['CumuleC']
+df['CumulA'] = df['A'].cumsum().map('{0:g}'.format)
+df['CumulB'] = df['B'].cumsum().map('{0:g}'.format)
+df['CumulC'] = df['C'].cumsum().map('{0:g}'.format)
+df['CumuleA'] = df['CumulA'] + df['CumulC']
+df['CumuleB'] = df['CumulB'] + df['CumulC']
 df.to_csv("./result.csv")
