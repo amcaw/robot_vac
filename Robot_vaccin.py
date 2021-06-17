72eda0907716494f1f2cf2e261830cd6a20b96f0
amcaw <42608053+amcaw@users.noreply.github.com>
2021-06-17 12:11:30 +0200

    Update Robot_vaccin.py

--- Robot_vaccin.py
+++ Robot_vaccin.py
@@ -1,77 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
-
-
-# In[2]:
-
-
 df = pd.read_csv('https://epistat.sciensano.be/Data/COVID19BE_VACC.csv')
-
-
-# In[3]:
-
-
 df = pd.pivot_table(df, index = 'DATE', columns = 'DOSE', values = 'COUNT', aggfunc = 'sum')
-
-
-# In[4]:
-
-
-print.head()
-
-
-# In[5]:
-
-
-head()
-
-
-# In[6]:
-
-
-df.head()
-
-
-# In[7]:
-
-
 df['CumuleA'] = df['A'].cumsum().map('{0:g}'.format)
 df['CumuleB'] = df['B'].cumsum().map('{0:g}'.format)
 df['CumuleC'] = df['C'].cumsum().map('{0:g}'.format)
-
-
-# In[8]:
-
-
-df.head()
-
-
-# In[9]:
-
-
 df["A"] = df['CumuleA'] + df['CumuleC']
 df["B"] = df['CumuleB'] + df['CumuleC']
-
-
-# In[10]:
-
-
-df.head()
-
-
-# In[11]:
-
-
-df.tail()
-
-
-# In[ ]:
-
-
 df.to_csv("/result.csv")
-
