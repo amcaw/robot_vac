0ed224bc4ee0bef595b5b350da33c15bd85f6739
amcaw <42608053+amcaw@users.noreply.github.com>
2021-06-20 23:01:22 +0200

    Update calcul.py

--- calcul.py
+++ calcul.py
@@ -2,7 +2,7 @@ import pandas as pd
 from datetime import datetime
 df = pd.read_csv ('https://epistat.sciensano.be/Data/COVID19BE_VACC.csv')
 df = pd.pivot_table(df, index = 'DATE', columns = 'DOSE', values = 'COUNT', aggfunc = 'sum')
-df['CumuleA'] = df['A'].cumsum().map('{0:g}'.format)
+df['CumuleA'] = df['A'].cumsum().map('{0:g}'.format) #yo
 df['CumuleB'] = df['B'].cumsum().map('{0:g}'.format)
 df['CumuleC'] = df['C'].cumsum().map('{0:g}'.format)
 df['DoseA'] = df['CumuleA'] + df['CumuleC']
