2a58e23f1fe7aebff65aa7e563a0decd90cfaebf
amcaw <42608053+amcaw@users.noreply.github.com>
2021-06-20 23:14:50 +0200

    Update calcul.py

--- calcul.py
+++ calcul.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import pandas as pd #yo
 from datetime import datetime
 df = pd.read_csv ('https://epistat.sciensano.be/Data/COVID19BE_VACC.csv')
 df = pd.pivot_table(df, index = 'DATE', columns = 'DOSE', values = 'COUNT', aggfunc = 'sum')
