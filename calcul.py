5f3da0e842a6d7e80bf7f3d06db1edc8cc13e3d3
amcaw <42608053+amcaw@users.noreply.github.com>
2021-07-29 20:05:57 +0200

    Update calcul.py

--- calcul.py
+++ calcul.py
@@ -1,12 +1,11 @@
 import pandas as pd #yoo
 from datetime import datetime
 df = pd.read_csv ('https://epistat.sciensano.be/Data/COVID19BE_VACC.csv')
+df = df[df.AGEGROUP != '0-17']
 df = pd.pivot_table(df, index = 'DATE', columns = 'DOSE', values = 'COUNT', aggfunc = 'sum')
-df['CumuleA'] = df['A'].cumsum().map('{0:g}'.format).fillna(0)
-df['CumuleB'] = df['B'].cumsum().map('{0:g}'.format).fillna(0)
-df['CumuleC'] = df['C'].cumsum().map('{0:g}'.format).fillna(0)
-df['DoseA'] = df['CumuleA'] + df['CumuleC']
-df['DoseB'] = df['CumuleB'] + df['CumuleC']
-df['DoseA'] = pd.to_numeric(df['DoseA'])
-df['DoseB'] = pd.to_numeric(df['DoseB'])
+df['CumuleA'] = df['A'].cumsum()
+df['CumuleB'] = df['B'].cumsum()
+df['CumuleC'] = df['C'].cumsum()
+df['A'] = df.apply(lambda x: x['CumuleA'] + x['CumuleC'], axis=1)
+df['B'] = df.apply(lambda x: x['CumuleB'] + x['CumuleC'], axis=1)
 df.to_csv("./result.csv")
