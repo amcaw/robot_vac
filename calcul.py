af75bfdf8978cd94718e4446f50650666a260518
amcaw <42608053+amcaw@users.noreply.github.com>
2021-08-30 15:14:34 +0200

    Update calcul.py

--- calcul.py
+++ calcul.py
@@ -8,6 +8,7 @@ df = pd.pivot_table(df, index = 'DATE', columns = 'DOSE', values = 'COUNT', aggf
 df['CumuleA'] = df['A'].cumsum()
 df['CumuleB'] = df['B'].cumsum()
 df['CumuleC'] = df['C'].cumsum()
+df['CumuleC'] = df['CumuleC'].fillna(0)
 df['A'] = df.apply(lambda x: x['CumuleA'] + x['CumuleC'], axis=1)
 df['B'] = df.apply(lambda x: x['CumuleB'] + x['CumuleC'], axis=1)
 df.to_csv("./result.csv")
